c713a4d66e0e656a4d2fdb2fa63298e3765af005
MateoRuiz123 <mrivera736@soy.sena.edu.co>
2023-06-26 00:09:33 -0500

    correcion en migracion de clientes

--- database/migrations/2023_06_12_141748_create_clientes_table.php
+++ database/migrations/2023_06_12_141748_create_clientes_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->bigInteger('numeroDocumento');
             $table->bigInteger('celular');
             $table->string('correo');
-            $table->Integer('estado')->default(1)->after('correo');
+            $table->Integer('estado')->default(1);
             $table->timestamps();
         });
     }
